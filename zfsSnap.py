5994de6c0eb0b258fb6e4080092d36659b4919c4
Unicorn Farts <unicornfarts@protonmail.com>
2024-01-20 13:05:12 -0800

    initial code push

---
+++ zfsSnap.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+from datetime import datetime
+from pprint import pprint
+import argparse, subprocess
+
+# configurable options
+zfsPool = 'rpool/ROOT/ubuntu_in1307'
+snapNameSchema = (zfsPool + '@_' + datetime.now().strftime('%Y-%m-%d') + '_')
+snapLimitDaily = 7
+snapLimitWeekly = 4
+snapLimitMonthly = 2
+
+def makeSnap(name):
+    subprocess.run(['/usr/bin/sudo', 'zfs', 'snap', name])
+
+def destroySnap(name):
+    subprocess.run(['/usr/bin/sudo', 'zfs', 'destroy', name])
+
+# populate snapshot lists
+dailySnaps, weeklySnaps, monthlySnaps = ([], [], [])
+result = subprocess.run(['/usr/sbin/zfs', 'list', '-t', 'snapshot'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+snaps = result.stdout.strip().split()[5::5]
+interval = len(snapNameSchema)
+for snapName in snaps:
+    if snapName[interval:] == 'daily':
+        dailySnaps.append(snapName)
+    elif snapName[interval:] == 'weekly':
+        weeklySnaps.append(snapName)
+    elif snapName[interval:] == 'monthly':
+        monthlySnaps.append(snapName)
+    else: # some other snap not part of this script
+        pass
+
+# set cmdline arguments/options
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--schedule', choices=['daily', 'weekly', 'monthly'], help='set snap name suffix')
+group.add_argument('--list', help='show current snapshots', action='store_true')
+args = parser.parse_args()
+
+if args.schedule: # crontab calls script using schedule argument
+    makeSnap((snapNameSchema + args.schedule))
+
+if args.list: # display current snapshots
+    for i in [dailySnaps, weeklySnaps, monthlySnaps]:
+        pprint(i)
+
+# cleanup old snaps // cleanup can also be accomplished by running script without arguments
+while len(dailySnaps) > snapLimitDaily:
+    destroySnap(dailySnaps[0])
+    del dailySnaps[0]
+while len(weeklySnaps) > snapLimitWeekly:
+    destroySnap(weeklySnaps[0])
+    del weeklySnaps[0]
+while len(monthlySnaps) > snapLimitMonthly:
+    destroySnap(monthlySnaps[0])
+    del monthlySnaps[0]
